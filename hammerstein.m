5471780215495a695914d4157e4fd1a3687c932b
spaghettiinhalers <122340592+spaghettiinhalers@users.noreply.github.com>
2025-03-28 12:30:02 -0400

    Update hammerstein.m

--- hammerstein.m
+++ hammerstein.m
@@ -1,5 +1,5 @@
 classdef hammerstein
-    %% setting up constants for hammerstein model
+
     %% Setting up constants for the Hammerstein model
     properties (Constant)
         % Input vector constants
@@ -23,14 +23,36 @@ classdef hammerstein
         % Output matrix
         C = [5436.56 0 -6795.7 0]
     end
+
     properties
-        PW {mustBeNumeric}
+        PW {mustBeNumeric} % pulsewidth
+        xk_bar (4,1) {mustBeNumeric}
     end
 
-%% approximation of isometric muscle recruitment curve determines peak value of muscle response for a given stimulus
-function u = IRC(c1, c2, PW)
-    u = c1 * abs(tanh(c2 * PW / 2));
-end
+    methods
+
+        function obj = hammerstein(PW, xk_bar)
+            obj.PW = PW;
+            obj.xk_bar = xk_bar;
+        end
+
+        % input vector
+        % approximation of isometric muscle recruitment curve determines peak value of muscle response for a given stimulus
+        function u_bar = u_bar(obj)
+        u_bar = [obj.c1_flex * abs(tanh(obj.c2_flex * obj.PW / 2));
+                 obj.c1_ext  * abs(tanh(obj.c2_ext  * obj.PW / 2))];
+        end
+
+        % state-space model of muscle activation dynamics
+        function xk1_bar = xk1_bar(obj) % x k+1 bar
+            u_bar = obj.u_bar();
+            xk1_bar = (obj.Phi * obj.xk_bar) + (obj.Gamma * u_bar);
+        end
+
+        function yk = yk(obj)
+            yk = obj.C * obj.xk_bar;
+        end
+
+    end
 
 end
-
