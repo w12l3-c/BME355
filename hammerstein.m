dabafb93929ca40d0072ce99ef6bc8aa0de0db41
longlong383 <122700749+longlong383@users.noreply.github.com>
2025-03-28 12:00:25 -0400

    Update hammerstein.m
    Turned hammerstein model from function call to class

--- hammerstein.m
+++ hammerstein.m
@@ -1,33 +1,31 @@
-function hammersteinOutput = hammersteinReturnValues(PW);
-
-%% setting up constants for hammerstein model
-
-% input vector
-c1_flex = 4.14;
-c2_flex = 2655.88;
-c1_ext = 4.75;
-c2_ext = 913.2;
-
-uf = IRC(c1_flex, c2_flex, PW);
-ue = IRC(c1_ext, c2_ext, PW);
-
-u_bar = [uf;
-         ue];
-
-% state matrix
-Phi = [0.82 0.008 0 0; 
-       0 0.82 0 0; 
-       0 0 0.78 0.008; 
-       0 0 0  0.78];
-
-% input matrix
-Gamma = [0 0; 
-         0 0.009; 
-         0 0; 
-         0 0.099];
-
-% output matrix
-C = [5436.56 0 -6795.7 0];
+classdef hammerstein
+    %% setting up constants for hammerstein model
+    %% Setting up constants for the Hammerstein model
+    properties (Constant)
+        % Input vector constants
+        c1_flex = 4.14
+        c2_flex = 2655.88
+        c1_ext  = 4.75
+        c2_ext  = 913.2
+       
+        % State matrix
+        Phi = [0.82 0.008 0 0;
+               0 0.82 0 0;
+               0 0 0.78 0.008;
+               0 0 0 0.78]
+        
+        % Input matrix
+        Gamma = [0 0;
+                 0 0.009;
+                 0 0;
+                 0 0.099]
+        
+        % Output matrix
+        C = [5436.56 0 -6795.7 0]
+    end
+    properties
+        PW {mustBeNumeric}
+    end
 
 %% approximation of isometric muscle recruitment curve determines peak value of muscle response for a given stimulus
 function u = IRC(c1, c2, PW)
@@ -35,3 +33,4 @@ function u = IRC(c1, c2, PW)
 end
 
 end
+
